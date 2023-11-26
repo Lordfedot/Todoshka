68f31cf21903d7571b03c3fde71aeb7a1b6f4135
LordFedot~ <you@example.com>
2023-11-26 17:43:08 +0200

    board page

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,8 +1,19 @@
+import { auth } from "@clerk/nextjs";
+
 import { OrgControl } from "./_components/org-control";
+import { startCase } from "lodash";
 
 type Props = {
   children: React.ReactNode;
 };
+
+export async function generateMetadata() {
+  const { orgSlug } = auth();
+
+  return {
+    title: startCase(orgSlug || "organization"),
+  };
+}
 
 const OrganizationIdLayout = ({ children }: Props) => {
   return (
